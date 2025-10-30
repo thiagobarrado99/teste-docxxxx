bb896faaeab94b2040bd62f0faf73df6b77f80b4
Thiago <thiagobarrado99@gmail.com>
2025-10-30 13:36:56 -0300

    Middleware, routes and sweetalert configs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ZipController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['forceauth'])->group(function () {
+        
+    Route::get('/', function () {
+        return redirect('/dashboard');
+    });
+
+    Route::middleware(['auth'])->prefix("dashboard")->controller(DashboardController::class)->group(function () {
+        //Routes available to all logged users
+        Route::get("/", "index");
+        Route::post("/logout", "logout");
+
+        Route::post('/keep-token-alive', 'keepTokenAlive');
+
+        Route::post("/view-notifications", "viewNotifications");
+
+        Route::prefix("zips")->controller(ZipController::class)->group(function () {
+            Route::get("/", "index");
+            Route::get("/create", "create");
+            Route::get("/{id}/edit", "edit");
+
+            Route::put("/{id}", "update");
+            Route::post("/", "store");
+
+            Route::delete("/{id}", "delete");
+        });
+    });
 });
