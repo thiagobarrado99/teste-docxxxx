9adaa63c3f786ecb2245e9e042fecb72fac74493
Thiago <thiagobarrado99@gmail.com>
2025-10-30 13:38:33 -0300

    Default dashboard page

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.dashboard')
+
+@section('header')
+    <i class="fas fa-chart-line"></i> Dashboard Teste Docteka
+@endsection
+
+@section('content')
+    Utilize as abas do lado esquerdo para navegar pelo sistema.
+@endsection
+
+@section('footer')
+    <p class="text-muted mb-0">Os dados são atualizados junto com a pagina.</p>
+@endsection
