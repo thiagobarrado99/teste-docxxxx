6dcc9b05166da64bdd7e03d0fec6758e12849fc0
Thiago <thiagobarrado99@gmail.com>
2025-10-31 01:26:10 -0300

    Pagination because there are too much records

--- app/Http/Controllers/ZipController.php
+++ app/Http/Controllers/ZipController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ImportCsvRequest;
 use App\Http\Requests\StoreZipRequest;
+use App\Jobs\ProcessCsvImportJob;
 use App\Models\Zip;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -12,7 +14,7 @@ class ZipController extends Controller
     //
     public function index(Request $request)
     {
-        $zips = Zip::all();
+        $zips = Zip::paginate(500);
         return view('dashboard.zips.index', compact('zips'));
     }
 
@@ -46,4 +48,23 @@ class ZipController extends Controller
         }
         return redirect("/dashboard/zips");
     }
+
+    public function upload(ImportCsvRequest $request)
+    {
+        // Dispatch a job for each file uploaded
+        foreach ($request->file('files') as $file) {
+            $path = $file->store('imports');
+
+            ProcessCsvImportJob::dispatch($path, Auth::user()->id);
+        }
+
+        Auth::user()->notifications()->create([
+            "title" => "<i class='fas fa-hourglass'></i> Arquivo em processamento",
+            "body" => "Os arquivos .CSV estão em processamento. Iremos lhe notificar assim que o processo for concluido."
+        ]);
+
+        toast("Arquivos recebidos com sucesso!", "success");
+        return redirect("/dashboard/zips");
+    }
+
 }
